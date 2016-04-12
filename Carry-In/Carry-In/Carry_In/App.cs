15df93c601b47704ad67e9ab25072347f2847171
Ooshybot-VM <chrisxzimmerman@gmail.com>
2016-04-12 19:49:34 -0400

    Added Home folder and HomePage file.

--- Carry-In/Carry-In/Carry_In/App.cs
+++ Carry-In/Carry-In/Carry_In/App.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 using Xamarin.Forms;
 
 namespace Carry_In
 {
-	public class App : Application
+    public class App : Application
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+            // The root page of your application
+            MainPage = new Home.HomePage();
 		}
 
 		protected override void OnStart ()
