bb253a40af46cd528e1ca64cf80bf80038bb9853
Chris Zimmerman <chrisxzimmerman@gmail.com>
2016-04-12 19:40:01 -0400

    Changed folder/file layout in shared project. I don't want to see Views/ViewModels/Models folders; instead, I'd like to see each component in its own folder. This may be further refined in the future.

---
+++ Carry-In/Carry-In/Carry_In/Login/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Carry_In.Login
+{
+    public partial class LoginPage : ContentPage
+    {
+        public string MainText { get; set; }
+        public LoginPage()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        private void Setup()
+        {
+            MainText = "Initial Text";
+        }
+    }
+}
